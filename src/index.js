1e0e09d974ac2cd466d93b00bab074d5560f2349
Östman Timo <timo.ostman@turkuamk.fi>
2019-05-31 10:16:48 +0300

    First commit

--- src/index.js
+++ src/index.js
@@ -1,12 +1,16 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+import store from './store/index';
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+import addArticle from './actions/index'
+
+console.log("getting state");
+console.log(store.getState());
+
+console.log("subscibing...");
+store.subscribe(() => console.log('Look ma, Redux!!'));
+
+console.log("dispatching....");
+store.dispatch( addArticle({ title: 'React Redux Tutorial for Beginners', id: 1 }) );
+store.dispatch( addArticle({ title: 'React Redux Tutorial for Beginners part 2', id: 2 }) );
+console.log("getting the final state...")
+console.log(store.getState().articles[1].title);
---
+++ src/js/index.js
@@ -0,0 +1,5 @@
+import store from '../store/index';
+
+import {addArticle} from '../actions/index';
+
+console.log(store.getState());
